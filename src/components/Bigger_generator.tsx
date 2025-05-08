1efcc89bed36ea5ec59ad4ebb45e801668ae686e
MatheusMarcelo01 <matheusmpmenezes@protonmail.com>
2025-05-08 16:52:57 -0300

    up

--- src/components/Bigger_generator.tsx
+++ src/components/Bigger_generator.tsx
@@ -148,7 +148,7 @@ const Gerador = () => {
 
   return (
     <div className="p-4 bg-gray-100 min-h-screen">
-      <h1 className="text-xl font-bold mb-4">Gerar Documento de Formalização da Demanda  (Modelo Maior - 3 folhas)</h1>
+      <h1 className="text-xl font-bold mb-4">Gerar Documento de Formalização da Demanda  (Modelo Maior - 3 páginas)</h1>
 
       
       <input
